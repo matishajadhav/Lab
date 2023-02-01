70ee9e66b967120c21157f6266b82acb7db5c9ca
matishajadhav <jadhavmatisha@gmail.com>
2023-02-01 16:19:51 +0530

    Updated

---
+++ cal.py
@@ -0,0 +1,6 @@
+x = int(input("Enter the value:"))
+y = int(input("Enter the second value:"))
+print("Addition=",x+y)
+print("Subtraction=",x-y)
+print("Multiplication=",x*y)
+print("Division=",x/y)
